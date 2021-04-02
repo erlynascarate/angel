97e7bdc484399bc827ebea6aab4d30f991eb8891
erlynascarate <erlyndavid@outlook.es>
2021-04-02 17:12:08 -0500

    Falta poner los iconos en "perfil", casi termini esa parte

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
-
-let contentNav = document.querySelector('.content-nav');
-let botonMenu = document.querySelector('.boton-menu');
+//Comienzo: desplegar y esconder el menu de navegacion
+let cajaNav = document.querySelector('.caja-nav');
+let botonMenu = document.querySelector('.contenido-header__boton');
 let nav = document.querySelector('nav');
 
-botonMenu.addEventListener('click', ()=>{
-	contentNav.classList.toggle('content-nav--aparecer');
+botonMenu.addEventListener('click', ()=>
+{
+	cajaNav.classList.toggle('caja-nav--aparecer');
 	nav.classList.toggle('nav--aparecer');
-});
+})
 
-contentNav.addEventListener('click', (e)=>{
-	if (e.target != nav && e.target == contentNav){
+cajaNav.addEventListener('click', (e)=>
+{
+	if (e.target != nav && e.target == cajaNav)
+	{
 		nav.classList.toggle('nav--aparecer');
-		contentNav.classList.toggle('content-nav--aparecer');
+		cajaNav.classList.toggle('caja-nav--aparecer');
 	}
-});
+})
+//Fin: desplegar y esconder el menu de navegacion
